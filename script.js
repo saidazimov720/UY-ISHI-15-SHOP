e5bc5809ac13894b9ebab3573815ef090762eafd
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-08-24 15:52:00 +0500

    edit the js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var sum = document.getElementById("sum");
 var item = document.getElementById("item");
-var totalSum = 0;
-var itemCount = 0;
+var items = [];
 
 function solve(cashAmount) {
-    totalSum += cashAmount;
-    itemCount++;
+    items.push(cashAmount);
+
+    var totalSum = items.reduce(function (acc, currentValue) {
+        return acc + currentValue;
+    }, 0);
+
+    var itemCount = items.length;
     
     sum.innerHTML = `<span>Total cost: ${totalSum.toLocaleString("uz")} so'm</span>`;
     item.innerHTML = `<span>Total items: ${itemCount}</span>`;
