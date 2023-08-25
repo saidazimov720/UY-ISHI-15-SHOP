e23210bed21be6b9c6e0852670a7f26e1f26a12c
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-08-25 21:21:13 +0500

    add the js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function solve(cashAmount) {
     sum.innerHTML = `<span>Total cost: ${totalSum.toLocaleString("uz")} so'm</span>`;
     item.innerHTML = `<span>Total items: ${itemCount}</span>`;
 }
+var sum = document.getElementById("sum");
+        var item = document.getElementById("item");
+        var items = [];
+
+        function solve(cashAmount) {
+            items.push(cashAmount);
+
+            var totalSum = items.reduce(function (acc, currentValue) {
+                return acc + currentValue;
+            }, 0);
+
+            var itemCount = items.length;
+            
+            sum.innerHTML = `<span>Total cost: ${totalSum.toLocaleString("uz")} so'm</span>`;
+            item.innerHTML = `<span>Total items: ${itemCount}</span>`;
+        }
